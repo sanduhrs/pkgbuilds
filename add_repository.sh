5161a657289f2d905e996f6149980a14e853641f
Johannes Schleifenbaum <johannes@js-webcoding.de>
2015-05-21 19:26:53 +0200

    AUR on default port

--- add_repository.sh
+++ add_repository.sh
@@ -14,13 +14,12 @@ fi
 
 AUR4HOST=${AUR4HOST:-aur-dev.archlinux.org}
 AUR4USER=${AUR4USER:-aur}
-AUR4PORT=${AUR4PORT:-2222}
 
 TEMP="$(mktemp -d --tmpdir aurpackage.XXXXX)"
 
 pushd "${TEMP}"
 git init
-git remote add origin "ssh+git://${AUR4USER}@${AUR4HOST}:${AUR4PORT}/${PACKAGE}.git"
+git remote add origin "ssh+git://${AUR4USER}@${AUR4HOST}/${PACKAGE}.git"
 echo "initialized new repository at ${TEMP} - Please add PKGBUILD and .SRCINFO."
 read
 git push -u origin master
@@ -29,5 +28,5 @@ popd
 rm -r "${TEMP}"
 
 ssh -p${AUR4PORT} ${AUR4USER}@${AUR4HOST} setup-repo "${PACKAGE}"
-git submodule add "ssh+git://${AUR4USER}@${AUR4HOST}:${AUR4PORT}/${PACKAGE}.git/" "${PACKAGE}"
+git submodule add "ssh+git://${AUR4USER}@${AUR4HOST}/${PACKAGE}.git/" "${PACKAGE}"
 git submodule update
